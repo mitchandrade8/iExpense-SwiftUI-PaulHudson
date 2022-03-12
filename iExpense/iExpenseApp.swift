452c355ac3c3aec161383674600a4e46d0c94aec
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-11 20:41:46 -0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Mitch Andrade on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
